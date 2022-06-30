1d1d60c32aad463b12583f655df6436d6e466430
Vedant Kalbag <jn.saksham@gmail.com>
2022-06-30 12:51:36 +0000

    evaluation with model and dataset selection modularity

--- Saksham/modelEval.py
+++ Saksham/modelEval.py
@@ -12,8 +12,10 @@ import matplotlib.pyplot as plt
 import json
 import os, pickle, numpy as np, pandas as pd
 from sklearn.metrics import confusion_matrix, f1_score, recall_score, accuracy_score
+import seaborn as sns
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=15, help='num epochs')
 parser.add_argument('--model', type=str, default='Xception', help='model')
 parser.add_argument('--gpu', type=str, default='0', help='GPU to use for training')
 parser.add_argument('--multigpu', type=int, default=0, help='Train on multiple GPUs')
@@ -23,7 +25,9 @@ parser.add_argument('--audioType', type=str, default = 'Augmented', help='Final 
 parser.add_argument('--a', type=float, default = 0.5, help='Blend factor')
 parser.add_argument('--min_snr', type=float, default = 1.0, help='Minimum snr for BG audio')
 parser.add_argument('--max_snr', type=float, default = 10.0, help='Maximum snr for BG audio')
-parser.add_argument('--batch', type=int, default = 32, help='Batch Size')
+parser.add_argument('--batch', type=int, default = 256, help='Batch Size')
+parser.add_argument('--datasetDir', type=str, default="../datasets/gtzan10sAug/datasets/", help="Main directory with subdirs of all generated datasets")
+parser.add_argument('--datasetName', type=str, default="test", help="Output path of augmented audio subdirectories. Do not add a '/' after dir name in input")
 config = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES']=config.gpu
@@ -31,15 +35,12 @@ print("Train on multiple gpus: ", bool(config.multigpu))
 print("Training on GPU",os.environ['CUDA_VISIBLE_DEVICES'])
 
 class AugmentedDataset(keras.utils.Sequence):
-    def __init__(self, mode, batch_size):
+    def __init__(self, datasetName, datasetDir, batch_size):
         self.batch_size = batch_size
-        self.mode = mode
-        if self.mode == 'train':
-            self.data_path = '../datasets/gtzan10sAug/vedant/train'
-        if self.mode == 'test':
-            self.data_path = '../datasets/gtzan10sAug/vedant/test'
-        if self.mode == 'val':
-            self.data_path = '../datasets/gtzan10sAug/vedant/val'
+        self.datasetDir = datasetDir
+        self.datasetName = datasetName
+        self.data_path = f'{self.datasetDir}{self.datasetName}/features/val/'
+        print ('Dataset path: ', self.data_path)
         _,_,self.filenames = next(os.walk(self.data_path))
 
     def __len__(self):
@@ -49,11 +50,12 @@ class AugmentedDataset(keras.utils.Sequence):
         X = np.empty((self.batch_size, 128,130))
         y = np.empty(self.batch_size)
         batch = self.filenames[idx * self.batch_size : (idx+1) * self.batch_size]
+        IDs = []
         for i, ID in enumerate(batch):
             tmp = np.load(os.path.join(self.data_path, ID), allow_pickle=True)
             X[i] = tmp[0]
             y[i] = tmp[1]
-
+            IDs.append(ID)
         # bat = np.array([np.load(os.path.join(self.data_path, x), allow_pickle=True) for x in batch])
         return X,y #bat[:,0], bat[:,1]
 
@@ -65,17 +67,11 @@ a = config.a
 min_snr = config.min_snr
 max_snr = config.max_snr
 batch_size = config.batch
-
-modelName = f'model_10s_{config.model}_{config.blockLength}block_{config.hopLength}slide.h5'
-modelPath = f'models/{modelName}'
-
-# Load eval data
-# folderDir = f'../datasets/gtzan10sAug/Final/features/'
-# dataPath = f'a{a}_min{min_snr}_max{max_snr}_{config.blockLength}s_block_{config.hopLength}s_hop.npy'
-
-savePath = '/models/'
-saveName = f'Pred_a{a}_min{min_snr}_max{max_snr}_{config.blockLength}s_block_{config.hopLength}s_hop.csv'
-# path = folderDir + dataPath
+audioType = config.audioType
+model = config.model
+datasetName = config.datasetName
+datasetDir = config.datasetDir
+epochs = config.epochs
 
 def gtruth(dataset):
     y = np.array([])
@@ -84,20 +80,33 @@ def gtruth(dataset):
     print ('Num Labels: ', len(y), y)
     return y
 
-def main(modelPath, savePath, savefname, a, min_snr, max_snr, batch_size):
+def saveCM(ConfusionMatrix, mapping, savePath):
+    ConfusionMatrix = pd.DataFrame(ConfusionMatrix)
+    ConfusionMatrix = ConfusionMatrix.rename(index = mapping, columns = mapping)
+
+    plt.rcParams['figure.figsize']=[15,5]
+    sns.set(font_scale=1.4)
+    sns_plot = sns.heatmap(ConfusionMatrix, cmap="YlGnBu", annot=True, annot_kws={"size":16});
+    fig = sns_plot.get_figure()
+    fig.savefig(savePath + ".png") 
+
+def main(modelPath, savePath, a, min_snr, max_snr, batch_size, datasetDir, datasetName, mapping):
     # Load the model
     model = keras.models.load_model(modelPath)
-    dataset = AugmentedDataset('train', batch_size)
+    
+    # Load dataset
+    dataset = AugmentedDataset(datasetName, datasetDir, batch_size)
 
     y = gtruth(dataset)
 
     y_pred = model.predict(dataset, batch_size = 32, verbose = 1)
     y_pred = np.argmax(y_pred, axis=-1)
-    print ('Total predictions: ', y_pred.shape)
-    np.savetxt('train_augmented_predictions.csv', y_pred)
+    # print ('Total predictions: ', y_pred.shape)
+    np.savetxt(savePath + ".csv", y_pred)
 
     # Get confusion matrix
     cm = confusion_matrix(y, y_pred)
+    saveCM(cm, mapping, savePath)
     f1 = f1_score(y, y_pred, average='weighted')
     recall = recall_score(y, y_pred, average='weighted')
     print (f'f1 score: {f1}, avg: {np.mean(f1)}')
@@ -111,7 +120,15 @@ if __name__ == '__main__':
     os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     
+    modelPath = f'models/model_10s_{audioType}_{model}_a{a}_min{min_snr}_max{max_snr}_{config.blockLength}block_{config.hopLength}slide_{config.epochs}epochs.h5'
+    # Path to save predictions
+    savePath = f'results/Pred_{audioType}train_{datasetName}eval_{model}_a{a}_min{min_snr}_max{max_snr}_{config.blockLength}block_{config.hopLength}slide_{config.epochs}epochs'
+    
+    mapping = {0: 'blues', 1: 'classical', 2: 'country', 3: 'disco', 4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae', 9: 'rock'}
+
     # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
     # os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
     
-    main(modelPath, savePath, saveName, a, min_snr, max_snr, batch_size)
+    # print (labels[:50])
+    main(modelPath, savePath, a, min_snr, max_snr, batch_size, datasetDir, datasetName, mapping)
+    
